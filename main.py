f20fab2b61302eeae34f8c2bc1757cd30cc0b634
Dragity Iasmina Georgiana <iasmina.dragity@student.upt.ro>
2021-05-17 21:17:50 +0300

    Am creat prima parte pentru interfata convertorului online.

--- main.py
+++ main.py
@@ -14,5 +14,49 @@ class RealTimeCurrencyConverter():
         if from_currency != 'EUR' :
             amount = amount / self.currencies[from_currency]
 
+        
         amount = round(amount * self.currencies[to_currency], 6)
         return amount
+
+class App(tk.Tk):
+
+    def __init__(self, converter):
+        tk.Tk.__init__(self)
+        self.title = 'Currency Converter'
+        self.currency_converter = converter
+
+        self.geometry("500x200")
+
+        # Frame
+        self.intro_label = Label(self, text = 'Bine ai venit pe convertorul online',  fg = 'blue', relief = tk.RAISED, borderwidth = 3)
+        self.intro_label.config(font = ('Courier',15,'bold'))
+
+        self.date_label = Label(self, text = f"1 RON = {self.currency_converter.convert('RON','EUR',1)} EUR \n Date : {self.currency_converter.data['date']}", relief = tk.GROOVE, borderwidth = 5)
+
+        self.intro_label.place(x = 10 , y = 5)
+        self.date_label.place(x = 160, y= 50)
+
+        
+        valid = (self.register(self.restrictNumberOnly), '%d', '%P')
+        self.amount_field = Entry(self,bd = 3, relief = tk.RIDGE, justify = tk.CENTER,validate='key', validatecommand=valid)
+        self.converted_amount_field_label = Label(self, text = '', fg = 'black', bg = 'white', relief = tk.RIDGE, justify = tk.CENTER, width = 17, borderwidth = 3)
+
+        
+        self.from_currency_variable = StringVar(self)
+        self.from_currency_variable.set("RON") 
+        self.to_currency_variable = StringVar(self)
+        self.to_currency_variable.set("EUR") 
+
+        font = ("Courier", 12, "bold")
+        self.option_add('*TCombobox*Listbox.font', font)
+        self.from_currency_dropdown = ttk.Combobox(self, textvariable=self.from_currency_variable,values=list(self.currency_converter.currencies.keys()), font = font, state = 'readonly', width = 12, justify = tk.CENTER)
+        self.to_currency_dropdown = ttk.Combobox(self, textvariable=self.to_currency_variable,values=list(self.currency_converter.currencies.keys()), font = font, state = 'readonly', width = 12, justify = tk.CENTER)
+
+
+if __name__ == '__main__':
+    url = 'https://api.exchangerate-api.com/v4/latest/USD'
+    converter = RealTimeCurrencyConverter(url)
+
+    App(converter)
+    mainloop()
+
