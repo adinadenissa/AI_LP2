0e51ec7b8f9a237a8559afdf99321f3bf1b42f1a
adinadenissa <84103849+adinadenissa@users.noreply.github.com>
2021-05-18 11:40:34 +0300

    Am creat a II-a parte a interfetei convertorului online.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class RealTimeCurrencyConverter():
         if from_currency != 'EUR' :
             amount = amount / self.currencies[from_currency]
 
-        
+
         amount = round(amount * self.currencies[to_currency], 6)
         return amount
 
@@ -36,21 +36,40 @@ class App(tk.Tk):
         self.intro_label.place(x = 10 , y = 5)
         self.date_label.place(x = 160, y= 50)
 
-        
+
         valid = (self.register(self.restrictNumberOnly), '%d', '%P')
         self.amount_field = Entry(self,bd = 3, relief = tk.RIDGE, justify = tk.CENTER,validate='key', validatecommand=valid)
         self.converted_amount_field_label = Label(self, text = '', fg = 'black', bg = 'white', relief = tk.RIDGE, justify = tk.CENTER, width = 17, borderwidth = 3)
 
-        
+
         self.from_currency_variable = StringVar(self)
-        self.from_currency_variable.set("RON") 
+        self.from_currency_variable.set("RON")
         self.to_currency_variable = StringVar(self)
-        self.to_currency_variable.set("EUR") 
+        self.to_currency_variable.set("EUR")
 
         font = ("Courier", 12, "bold")
         self.option_add('*TCombobox*Listbox.font', font)
         self.from_currency_dropdown = ttk.Combobox(self, textvariable=self.from_currency_variable,values=list(self.currency_converter.currencies.keys()), font = font, state = 'readonly', width = 12, justify = tk.CENTER)
         self.to_currency_dropdown = ttk.Combobox(self, textvariable=self.to_currency_variable,values=list(self.currency_converter.currencies.keys()), font = font, state = 'readonly', width = 12, justify = tk.CENTER)
+        
+        self.from_currency_dropdown.place(x = 30, y= 120)
+        self.amount_field.place(x = 36, y = 150)
+        self.to_currency_dropdown.place(x = 340, y= 120)
+        self.converted_amount_field_label.place(x = 346, y = 150)
+
+        self.convert_button = Button(self, text = "Convert", fg = "black", command = self.perform)
+        self.convert_button.config(font=('Courier', 10, 'bold'))
+        self.convert_button.place(x = 225, y = 135)
+
+    def perform(self):
+        amount = float(self.amount_field.get())
+        from_curr = self.from_currency_variable.get()
+        to_curr = self.to_currency_variable.get()
+
+        converted_amount = self.currency_converter.convert(from_curr,to_curr,amount)
+        converted_amount = round(converted_amount, 2)
+
+        self.converted_amount_field_label.config(text = str(converted_amount))
 
 
 if __name__ == '__main__':
